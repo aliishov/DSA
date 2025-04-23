d5402fb0738d9ce5114e5e0dd89829e9b5b011c9
aliishov <alishov7394@gmail.com>
2025-04-23 18:38:43 +0400

    Binary To Decimal | LinkedList LeetCode Interview Question

--- src/datastructure/linkedlist/LinkedList.java
+++ src/datastructure/linkedlist/LinkedList.java
@@ -298,4 +298,42 @@ public class LinkedList {
             current = current.next;
         }
     }
+
+    // Binary To Decimal
+    public int binaryToDecimal() {
+        Node current = head;
+        int num = 0;
+
+        while (current != null) {
+            num = num * 2 + current.value;
+            current = current.next;
+        }
+
+        return num;
+    }
+
+    // Reverse Between
+    public void reverseBetween(int startIndex, int endIndex) {
+        if (head == null) return;
+
+        Node dummyNode = new Node(0);
+        dummyNode.next = head;
+        Node previousNode = dummyNode;
+
+        for (int i = 0; i < startIndex; i++) {
+            previousNode = previousNode.next;
+        }
+
+        Node currentNode = previousNode.next;
+
+        for(int i = 0; i < endIndex - startIndex; i++) {
+            Node nodeToMove = currentNode.next;
+            currentNode.next = nodeToMove.next;
+            nodeToMove.next = previousNode.next;
+            previousNode.next = nodeToMove;
+        }
+
+        head = dummyNode.next;
+
+    }
 }
