a11c1b057e1ecd34619b176552ad3faaa02f1234
aliishov <alishov7394@gmail.com>
2025-04-23 13:18:17 +0400

    Has Loop | LinkedList LeetCode Interview Question

--- src/datastructure/linkedlist/LinkedList.java
+++ src/datastructure/linkedlist/LinkedList.java
@@ -200,4 +200,17 @@ public class LinkedList {
         return slow;
     }
 
+    // Hash Loop
+    public boolean hasLoop() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
 }
