6b6b0135ff1f95bffd012521d5faa47eb4590d4f
aliishov <alishov7394@gmail.com>
2025-04-27 13:05:18 +0400

    Reverse String | Stack Interview Question

--- src/datastructure/stack/Main.java
+++ src/datastructure/stack/Main.java
@@ -1,7 +1,31 @@
 package datastructure.stack;
 
 public class Main {
+
+    // Reverse String
+    public static String reverseString(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+
+        Stack2<Character> stack = new Stack2<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+        }
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        return reversed;
+    }
+
     public static void main(String[] args) {
+        String myString = "hello";
+        String reversedString = reverseString(myString);
+        System.out.println(reversedString);
 
     }
 }
