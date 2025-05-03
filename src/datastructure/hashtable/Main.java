bf7cf5e613cb3b185d95f0b2c54b17f8d33f9ca1
aliishov <alishov7394@gmail.com>
2025-05-03 15:55:34 +0400

    Group Anagrams | HashTable Interview Question

--- src/datastructure/hashtable/Main.java
+++ src/datastructure/hashtable/Main.java
@@ -65,4 +65,21 @@ public class Main {
 
         return null;
     }
+
+    // Group Anagrams
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedString = new String(charArray);
+
+            if (!res.containsKey(sortedString)) {
+                res.put(sortedString, new ArrayList<>());
+            }
+            res.get(sortedString).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
 }
