7a272c97ae650f8c249c63fb089e4236590cad5a
aliishov <alishov7394@gmail.com>
2025-05-03 15:28:36 +0400

    Item In Common | HashTable Interview Question

--- src/datastructure/hashtable/Main.java
+++ src/datastructure/hashtable/Main.java
@@ -1,7 +1,23 @@
 package datastructure.hashtable;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    // Item In Common
+    public static boolean itemInCommon(int[] array1, int[] array2) {
+        HashMap<Integer, Boolean> myHashMap = new HashMap<>();
+
+        for (int i : array1) {
+            myHashMap.put(i, true);
+        }
+
+        for (int j : array2) {
+            if (myHashMap.get(j) != null) return true;
+        }
+        return false;
     }
 }
