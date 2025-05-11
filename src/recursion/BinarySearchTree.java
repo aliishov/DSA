6c9d881c44df26c7fdf45015baf14c1be4a4a927
aliishov <alishov7394@gmail.com>
2025-05-11 15:00:36 +0400

    Invert Binary Tree | Interview Question

--- src/recursion/BinarySearchTree.java
+++ src/recursion/BinarySearchTree.java
@@ -89,33 +89,6 @@ public class BinarySearchTree {
         return root;
     }
 
-    public List<Integer> inorderTraversal() {
-        List<Integer> result = new ArrayList<>();
-        inorderHelper(this.root, result);
-        return result;
-    }
-
-    private void inorderHelper(Node node, List<Integer> result) {
-        if (node == null) return;
-        inorderHelper(node.left, result);
-        result.add(node.value);
-        inorderHelper(node.right, result);
-    }
-
-    public boolean isBalanced() {
-        return height(this.root) != -1;
-    }
-
-    private int height(Node node) {
-        if (node == null) return 0;
-        int leftHeight = height(node.left);
-        if (leftHeight == -1) return -1;
-        int rightHeight = height(node.right);
-        if (rightHeight == -1) return -1;
-        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
-        return 1 + Math.max(leftHeight, rightHeight);
-    }
-
     public void sortedArrayToBST(int[] nums) {
         this.root = sortedArrayToBST(nums, 0, nums.length - 1);
     }
@@ -126,6 +99,21 @@ public class BinarySearchTree {
         Node node = new Node(nums[mid]);
         node.left = sortedArrayToBST(nums, left, mid - 1);
         node.right = sortedArrayToBST(nums, mid + 1, right);
+        return node;
+    }
+
+    // Invert Binary Tree
+    public void invert() {
+        root = invertTree(root);
+    }
+
+    private Node invertTree(Node node) {
+        if (node == null) return null;
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        invertTree(node.left);
+        invertTree(node.right);
         return node;
     }
 }
