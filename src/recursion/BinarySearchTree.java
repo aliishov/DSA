19d5b646842f95b6974e62a7c6c4445fc5575cec
aliishov <alishov7394@gmail.com>
2025-05-12 12:25:27 +0400

    Breadth First Search

--- src/recursion/BinarySearchTree.java
+++ src/recursion/BinarySearchTree.java
@@ -1,7 +1,9 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinarySearchTree {
     class Node {
@@ -115,5 +117,22 @@ public class BinarySearchTree {
         invertTree(node.left);
         invertTree(node.right);
         return node;
+    }
+
+    // Breadth First Search
+    public ArrayList<Integer> BFS() {
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(currentNode);
+
+        while (!queue.isEmpty()) {
+            currentNode = queue.remove();
+            results.add(currentNode.value);
+            if (currentNode.left != null) queue.add(currentNode.left);
+            if (currentNode.right != null) queue.add(currentNode.right);
+        }
+
+        return results;
     }
 }
