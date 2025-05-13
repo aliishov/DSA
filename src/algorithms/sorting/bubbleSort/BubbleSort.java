6000b6c82e4428f1e46a5f3e0ed640797c855c6c
aliishov <alishov7394@gmail.com>
2025-05-13 11:17:47 +0400

    Bubble Sort | Sorting Algorithms

---
+++ src/algorithms/sorting/bubbleSort/BubbleSort.java
@@ -0,0 +1,23 @@
+package algorithms.sorting.bubbleSort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 4, 2, 3, 5};
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
