095211e2f9fbe6c3e79c6fe167a8948ac0075326
aliishov <alishov7394@gmail.com>
2025-05-18 13:55:25 +0400

    Rotate | Array Interview Question

--- src/algorithms/array/Main.java
+++ src/algorithms/array/Main.java
@@ -101,6 +101,26 @@ public class Main {
         return maxProfit;
     }
 
+    // Rotate
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // если k > n
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args) {
 
         // removeElement Test case
@@ -139,5 +159,11 @@ public class Main {
         int[] prices1 = {7, 1, 5, 3, 6, 4};
         int profit1 = maxProfit(prices1);
         System.out.println("Test case 1: Maximum profit: " + profit1);
+
+        // rotate Test case
+        int[] nums4 = {1, 2, 3, 4, 5, 6, 7};
+        int k1 = 3;
+        rotate(nums4, k1);
+        System.out.println("Test case 1: Rotated array: " + Arrays.toString(nums4));
     }
 }
